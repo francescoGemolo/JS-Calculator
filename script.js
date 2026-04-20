c657c27bf5edc1f67361f1a82752e18ef1abac3c
Francesco <francescolavoro0209@gmail.com>
2026-04-20 12:35:50 +0200

    Dark Mode

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Ciao!");
+// Dark Mode
+const header = document.querySelector('.header');
+const iconOff = document.querySelector('.hgi-lightbulb-off');
+const iconOn = document.querySelector('.hgi-idea');
+
+header.addEventListener('click', () => {
+    iconOff.classList.toggle('hidden');
+    iconOn.classList.toggle('hidden');
+});
