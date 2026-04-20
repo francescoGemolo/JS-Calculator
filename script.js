c715b166d2b6b9902f2b3421326d45253f6ffaa2
Francesco <francescolavoro0209@gmail.com>
2026-04-20 15:39:46 +0200

    Update

--- script.js
+++
@@ -1,9 +0,0 @@
-// Dark Mode
-const header = document.querySelector('.header');
-const iconOff = document.querySelector('.hgi-lightbulb-off');
-const iconOn = document.querySelector('.hgi-idea');
-
-header.addEventListener('click', () => {
-    iconOff.classList.toggle('hidden');
-    iconOn.classList.toggle('hidden');
-});
