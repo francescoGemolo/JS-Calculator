c89aa0865f4dc869fd60326ba992edf7bc54248c
Francesco <francescolavoro0209@gmail.com>
2026-04-20 22:37:57 +0200

    Finished UI

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Dark Mode
 const container = document.querySelector('.dark-mode-container');
 const iconOn = document.querySelector('.hgi-idea');
 const iconOff = document.querySelector('.hgi-lightbulb-off');
 
 container.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+
     iconOn.classList.toggle('hidden');
     iconOff.classList.toggle('hidden');
 });
