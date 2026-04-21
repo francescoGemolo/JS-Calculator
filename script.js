a208573a59851d9256c7cfd3ae6d8de3d2a2b762
Francesco <francescolavoro0209@gmail.com>
2026-04-21 12:03:55 +0200

    Update UI, Basic JavaScript

--- script.js
+++ script.js
@@ -9,3 +9,42 @@ container.addEventListener('click', () => {
     iconOn.classList.toggle('hidden');
     iconOff.classList.toggle('hidden');
 });
+
+// Access DOM elements of the calculator
+const inputBox = document.getElementById('input');
+const expressionDiv = document.getElementById('expression');
+const resultDiv = document.getElementById('result');
+
+// Define expression and result variable
+let expression = '';
+let result = '';
+
+// Define event handler for button clicks
+function buttonClick(event) {
+    const target = event.target;
+    const action = target.dataset.action;
+    const value = target.dataset.value;
+
+    // Switch case to control the calculator
+    switch (action) {
+        case 'number':
+            addValue(value);
+            break;
+        case 'clear':
+            break;
+    }
+
+    // Update display
+    updateDisplay(expression, result);
+}
+
+inputBox.addEventListener('click', buttonClick);
+
+function addValue(value) {
+    expression = expression + value;
+}
+
+function updateDisplay(expression, result) {
+    expressionDiv.textContent = expression;
+    resultDiv.textContent = result;
+}
