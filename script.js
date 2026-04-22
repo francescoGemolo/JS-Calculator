250ec927ac70775a01d1920fa1f357e1ec8b49cd
Francesco <francescolavoro0209@gmail.com>
2026-04-22 15:27:33 +0200

    Update

--- script.js
+++ script.js
@@ -23,3 +23,44 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => (b === 0 ? "Really?" : a / b);
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+": return add(a, b);
+    case "-": return subtract(a, b);
+    case "*": return multiply(a, b);
+    case "/": return divide(a, b);
+    default: return null;
+  }
+}
+
+function updateDisplay() {
+  expressionDiv.textContent = firstNumber;
+  if (currentOperator) expressionDiv.textContent += ` ${currentOperator}`;
+  if (secondNumber) expressionDiv.textContent += ` ${secondNumber}`;
+  
+  // Scroll to end for long expressions
+  expressionDiv.scrollLeft = expressionDiv.scrollWidth;
+}
+
+function clear() {
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = null;
+  resultDiv.textContent = "";
+  updateDisplay();
+}
+
+function handleNumber(num) {
+  if (shouldResetDisplay) {
+    firstNumber = num;
+    shouldResetDisplay = false;
+  } else if (!currentOperator) {
+    firstNumber += num;
+  } else {
+    secondNumber += num;
+  }
+  updateDisplay();
+}
